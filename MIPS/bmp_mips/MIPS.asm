2566455b96c2c47a959fc795882684bbb9aec706
Richard Staszkiewicz <richard.staszkiewicz@gmail.com>
2021-04-25 22:30:53 +0200

    With fully functional get_pixel (colour) function. Reamins - getters & post check

--- MIPS/bmp_mips/MIPS.asm
+++ MIPS/bmp_mips/MIPS.asm
@@ -16,7 +16,7 @@
 reserved:	.space 2				# reserved space (for safety)
 image:	.space BMP_FILE_SIZE		# space to store the BMP file
 used:	.space 76800			# space to store the map of checked pixels
-fname:	.asciiz "input1clean.bmp"		# name of the imported file
+fname:	.asciiz "test.bmp"			# name of the imported file
 err_msg:	.asciiz "Error occured during run"	# Message printed if error
 
 
@@ -27,11 +27,10 @@ main:
 	jal 	clear_used		# clear the map of used (1-> used; 0-> unused)
 
 	# execute main program loop, starting with point (0, 0)
-	li 	$a0, -1			# x coordinate (initialized with 1 less)
 	li 	$a1, -1			# y coordinate (initialized with 1 less)
-
 loop_y:	addiu	$a1, $a1, 1
  	beq	$a1, 240, exit
+ 	li 	$a0, -1			# x coordinate (initialized with 1 less)
 loop_x:	addiu	$a0, $a0, 1
 	jal 	find_marker		# Check if marker on pixel (x, y) = ($a0, $a1) under adress $a2.
 	bne	$a0, 319, loop_x
@@ -105,8 +104,10 @@ clear_used:
 #description: resets the space alocated to store used pixels information
 #arguments: none
 #return value: none
-	subu 	$sp, $sp, 4		
-	sw 	$ra,4($sp)
+	subu 	$sp, $sp, 4
+	sw 	$ra, 4($sp)
+	subu 	$sp, $sp, 4
+	sw 	$s1, 4($sp)
 	
 	li	$t0, 0				# $t0 will store an iterator
 	la	$t1, used				# $t1 will store current byte
@@ -117,29 +118,83 @@ loop:	sb	$t3, ($t1)
 	addiu	$t1, $t1, 1
 	bne	$t0, 76800, loop
 	
-	jr	$ra
+	lw 	$s1, 4($sp)
+	addu 	$sp, $sp, 4
+	lw 	$ra, 4($sp)		
+	addu 	$sp, $sp, 4
+	jr 	$ra
 
 
 #===============find_marker=====================
 find_marker:
-#description: assumes (x, y) as left-down norner of the marker. Checks if it is correct, if so printing it's coordinates
-#arguments:
+# description: assumes (x, y) as left-down norner of the marker. Checks if it is correct, if so printing it's coordinates
+# arguments:
 #	$a0 - x coordinate
 #	$a1 - y coordinate
-#return value: none
-	subu 	$sp, $sp, 4		
-	sw 	$ra,4($sp)
+# return value: none
+	subu 	$sp, $sp, 4
+	sw 	$ra, 4($sp)
+	subu 	$sp, $sp, 4
+	sw 	$s1, 4($sp)
+	subu	$sp, $sp, 4
+	sw	$a1, 4($sp)
+	subu	$sp, $sp, 4
+	sw	$a0, 4($sp)
 	
+	# pixel adress in used
 	mul	$t0, $a1, BYTES_PER_ROW_USED
 	addu	$t0, $a0, $t0
 	la	$t1, used	
 	addu	$t1, $t0, $t1			# $t1 stores current pixels address in used
 	lb	$t0, ($t1)			# $t0 stores current pixels value in used
-	beq	$t0, 0, end_pix			# if the pixel is already used, proceed
+	beq	$t0, 1, end_pix			# if the pixel is already used, return
+	move	$s0, $t1				# save pixel adress in used to $s0
+	
+	# pixel address in image calculation
+	la 	$t1, image + 10			# adress of file offset to pixel array
+	lw 	$t2, ($t1)			# file offset to pixel array in $t2
+	la 	$t1, image			# adress of bitmap
+	add 	$t2, $t1, $t2			# adress of pixel array in $t2
+	mul 	$t1, $a1, BYTES_PER_ROW
+	move 	$t3, $a0	
+	sll 	$a0, $a0, 1
+	addu 	$t3, $t3, $a0
+	addu	$t1, $t1, $t3
+	addu 	$t2, $t2, $t1
+	move	$s1, $t2				# save pixel adress in image to $s1
+	move	$a2, $s1				# load pixels address in image to $a2
 	
 	
+	jal	get_pixel
+	li	$t0, 16777215
+	beq	$v0, $t0, end_pix			# if the pixel is white, continue
+	move	$s2, $v0				# save pixel RGB in $s2
+	
+	move	$a2, $s1				# save pixel address in image in $a2
+	move	$a3, $s2				# save pixel RGB in $a3
+#	jal	get_len
+#	beq	$v1, 1, end_pix
+#	move	$s1, $v0
+#	
+#	jal	get_hight
+#	beq	$v1, 1, end_pix
+#	move	$s2, $v0
+	
+#	jal	get_hight
+#	beq	$v1, 1, end_pix
+#	move	$s3, $v0
+	
 
-end_pix:	jr	$ra
+
+end_pix:	lw 	$a0, 4($sp)
+	addu 	$sp, $sp, 4
+	lw 	$a1, 4($sp)
+	addu 	$sp, $sp, 4
+	lw 	$s1, 4($sp)
+	addu 	$sp, $sp, 4
+	lw 	$ra, 4($sp)
+	addu 	$sp, $sp, 4
+	jr 	$ra
 
 
 
@@ -158,7 +213,7 @@ end_pix:	jr	$ra
 
 
 
-
+#=============================getters================================
 
 get_pixel:
 #description: 
@@ -166,37 +221,76 @@ get_pixel:
 #arguments:
 #	$a0 - x coordinate
 #	$a1 - y coordinate - (0,0) - bottom left corner
+#	$a2 - pixels adress in image
 #return value:
 #	$v0 - 0RGB - pixel color
 
-	sub $sp, $sp, 4		#push $ra to the stack
-	sw $ra,4($sp)
+	subu 	$sp, $sp, 4	#push $ra to the stack
+	sw 	$ra, 4($sp)
 
-	la $t1, image + 10	#adress of file offset to pixel array
-	lw $t2, ($t1)		#file offset to pixel array in $t2
-	la $t1, image		#adress of bitmap
-	add $t2, $t1, $t2	#adress of pixel array in $t2
+#	la 	$t1, image + 10	# adress of file offset to pixel array
+#	lw 	$t2, ($t1)	# file offset to pixel array in $t2
+#	la 	$t1, image	# adress of bitmap
+#	add	$t2, $t1, $t2	# adress of pixel array in $t2
 	
-	#pixel address calculation
-	mul $t1, $a1, BYTES_PER_ROW #t1= y*BYTES_PER_ROW
-	move $t3, $a0		
-	sll $a0, $a0, 1
-	add $t3, $t3, $a0	#$t3= 3*x
-	add $t1, $t1, $t3	#$t1 = 3x + y*BYTES_PER_ROW
-	add $t2, $t2, $t1	#pixel address 
+	move	$t2, $a2		# pixel address in image
 	
 	#get color
-	lbu $v0,($t2)		#load B
-	lbu $t1,1($t2)		#load G
-	sll $t1,$t1,8
-	or $v0, $v0, $t1
-	lbu $t1,2($t2)		#load R
-        sll $t1,$t1,16
-	or $v0, $v0, $t1
+	lbu 	$v0,($t2)			# load B
+	lbu 	$t1,1($t2)		# load G
+	sll 	$t1,$t1,8
+	or 	$v0, $v0, $t1
+	lbu 	$t1,2($t2)		# load R
+          sll 	$t1,$t1,16
+	or 	$v0, $v0, $t1
 					
-	lw $ra, 4($sp)		#restore (pop) $ra
-	add $sp, $sp, 4
-	jr $ra
+	lw 	$ra, 4($sp)		# restore (pop) $ra
+	addu 	$sp, $sp, 4
+	jr 	$ra
+
+
+get_len:
+# description: 
+#	returns potential length of single-coloured arm, starting with given coordinates pixel
+# arguments:
+#	$a0 - x coordinate
+#	$a1 - y coordinate
+#	$a2 - RGB signature to look for
+#	$a3 - Pixel address in image
+# return value:
+#	$v0 - counted length
+#	$v1 - 0 if fulfiles conditions, 1 otherwise
+	
+	subu 	$sp, $sp, 4		# push $ra to the stack
+	sw	$ra, 4($sp)
+	subu 	$sp, $sp, 4		# push $s0 (used adress) to the stack
+	sw 	$s0, 4($sp)
+	subu 	$sp, $sp, 4		# push $s1 (image adress) to the stack
+	sw 	$s0, 4($sp)
+	subu 	$sp, $sp, 4		# push $s2 (RGB) to the stack
+	sw 	$s0, 4($sp)
+	
+	li	$v1, 0			# at first, everything is OK
+	move	$t0, $a2			# current RGB
+	move	$t1, $a0			# real x coordinate
+	move	$t2, $s1			# current address in image
+	
+	
+check:	addu	$t2, $t2, 3
+	addiu	$a0, $t0, 1
+	beq	$a0, BYTES_PER_ROW, end_len
+	jal	get_pixel
+	beq	$t0, $v0, check
+	
+	
+
+
+end_len:	subu	$v0, $a0, $t1
+	lw 	$s0, 4($sp)
+	addu	$sp, $sp, 4
+	lw	$ra, 4($sp)
+	sw	$s0, 4($sp)
+	jr	$ra
 
 
 
