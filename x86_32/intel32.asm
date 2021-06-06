43907b427c31ea4365c3226b26eb5874b1cd69ea
Richard <rstaszki@gitlab-stud.elka.pw.edu.pl>
2021-06-06 21:02:20 +0200

    Past & copy get_len from MIPS. Seg fault to be debugged

--- x86_32/intel32.asm
+++ x86_32/intel32.asm
@@ -37,25 +37,25 @@
 ;  ---------------------------------
 ;  | saved ebp                     | EBP, ESP (find markers)
 ;  ---------------------------------
-;  | amount                        | EBP-4
+;  | amount                        |
 ;  ---------------------------------
-;  | *used  -> beginning           | EBP-8				EBP+36
+;  | *used  -> beginning           | EBP+36
 ;  ---------------------------------
-;  | *image -> beginning           | EBP-12				EBP+32
+;  | *image -> beginning           | EBP+32
 ;  ---------------------------------
-;  | width  -> const.              | EBP-16				EBP+28
+;  | width  -> const.              | EBP+28
 ;  ---------------------------------
-;  | height -> const.              | EBP-20				EBP+24
+;  | height -> const.              | EBP+24
 ;  ---------------------------------
-;  | curr_y -> const.              | EBP-24				EBP+20
+;  | curr_y -> const.              | EBP+20
 ;  ---------------------------------
-;  | curr_x -> const.              | EBP-28				EBP+16
+;  | curr_x -> const.              | EBP+16
 ;  ---------------------------------
-;  | curr_used -> flex.            | EBP-32				EBP+12
+;  | curr_used -> flex.            | EBP+12
 ;  ---------------------------------
-;  | curr_img -> flex.             | EBP-36				EBP+8
+;  | curr_img -> flex.             | EBP+8
 ;  ---------------------------------
-;  | return address (mainL)        | EBP-40				EBP+4
+;  | return address (mainL)        | EBP+4
 ;  ---------------------------------
 ;  | saved ebp                     | EBP, ESP (find_marker)
 ;  ---------------------------------
@@ -66,6 +66,12 @@
 ;  | curr_used -> flex.            | EBP-12
 ;  ---------------------------------
 ;  | curr_img -> flex.             | EBP-16
+;  ---------------------------------
+;  | marker_len -> flex.           | EBP-20
+;  ---------------------------------
+;  | marker_width -> flex.         | EBP-24
+;  ---------------------------------
+;  | marker_hgh-> flex.            | EBP-28
 ;  ---------------------------------
 ;
 ;
@@ -200,6 +206,7 @@ find_marker:
 	push	eax					;initiate marker_len (int32)
 	push	eax					;initiate marker_width (int32)
 	push	eax					;initiate marker_hgh (int32)
+	mov		edx, 1				;PRINT FLAG set to true
 
 	mov		eax, [ebp + 8]
 	push	eax					;push current Image pos (get_pixel arg)
@@ -210,25 +217,39 @@ find_marker:
 	call	get_pixel
 	cmp		eax, 0
 	jne		exit_fm				;if pixel is not black (0x000000) jump to end
-
 	pop		eax					;take off X from stack
 	pop		eax					;take off Y from stack
 	pop		eax					;take off Image pos from stack
 
+	;markers potential length
+	jmp		get_len
+	and		edx, ecx			;bitwise AND via print flag
+	mov		[ebp - 20], eax		;move result to proper place on stack
+
+
 exit_fm:
-	mov		eax, 1				;return 1
+	mov		eax, edx			;return edx
 
 	mov		esp, ebp
 	pop		ebp
 	ret
 
+;==========================================================================
+;============================EXTRA  FUNCTIONS =============================
+;=========================================================================
 
 get_pixel:
-; arguments: on stack ([ebp+8] -> x, [ebp+12] -> y, [ebp+16] -> image_pos)
-; return: Pixel colour in EAX 0x00BBGGRR
+; description: return chosen pixels RGB
+; arguments: on stack
+; 			[ebp+8] -> x
+; 			[ebp+12] -> y
+;			[ebp+16] -> image_pos
+; return:
+; 		EAX -> Pixel colour 0x00BBGGRR
 	push	ebp
 	mov 	ebp, esp
 	push	ebx
+	push	ecx
 
 	mov		ebx, [ebp + 16]		;pixel adress in image to EBX
 	movzx	eax, BYTE [ebx]			;load B
@@ -239,6 +260,89 @@ get_pixel:
 	shl		ecx, 16
 	or		eax, ecx			;accumulate
 
+	pop		ecx
+	pop		ebx
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+
+get_len:
+; description: return potential length of arm, starting with given coordinates pixel
+; arguments: on stack
+; 			[ebp+28] -> [ebp-4] -> x
+;			[ebp+20] -> [ebp-8] -> image_pos
+;			[ebp+24] -> [ebp-12] -> used_pos
+;			[ebp+64] -> width const.
+;			[ebp+32] -> y
+; return:
+; 		EAX -> counted length
+;		ECX -> 1 executed, 0 error
+
+	push	ebp
+	mov		ebp, esp
+	mov		eax, [ebp + 28]
+	push	eax 				;push posx
+	mov		eax, [ebp + 20]
+	push	eax					;push image_pos
+	mov		eax, [ebp + 24]
+	push	eax					;push used_pos
+	push	ebx
+	push	edx
+
+	mov 	ecx, 0		;At first, ERROR FLAG is not set
+
+check_l:
+	mov		ebx, [ebp - 4]
+	inc		ebx
+	mov		[ebp - 4], ebx	;increment posx
+	mov		ebx, [ebp - 8]
+	add		ebx, 3
+	mov		[ebp - 8], ebx	;increment pos in image
+	mov		ebx, [ebp - 12]
+	inc		ebx
+	mov		[ebp - 12], ebx	;increment pos in used
+	mov		ebx, [ebp - 4]
+	cmp		ebx, [ebp + 64]
+	je		end_len			;if the X is the border, end
+
+	mov 	ebx, [ebp - 12]
+	mov		edx, [ebx]		;used value
+	add		ecx, edx		;in ERROR FLAG will be stored the sum of used pixels
+	mov		[ebp - 12], DWORD 1	;set pixel as used
+
+	push	eax
+	mov		ebx, [ebp - 8]
+	push	ebx				;push image pos on stack (get_pixel)
+	mov		ebx, [ebp + 32]
+	push	ebx				;push ypos on stack (get_pixel)
+	mov		ebx, [ebp - 4]
+	push 	ebx				;push xpos on stack (get_pixel)
+	call	get_pixel
+	cmp		eax, 0
+	pop		eax				;pop xpos
+	pop		eax				;pop ypos
+	pop		eax				;pop image pos
+	pop		eax				;return to original EAX
+	je		check_l
+
+	cmp		edx, 1			;if last pixel was used, it is irrelevant
+	jne		end_len			;last pixel was not used
+	dec		ecx
+
+
+end_len:
+	mov		eax, [ebp - 4]	;ending x
+	mov		ebx, [ebp + 28]	;orginal x
+	sub		eax, ebx
+	dec		eax				;x = (ex - ox) - 1
+	cmp		ecx, 0
+	mov		ecx, 1
+	je		exit_len		; if ecx was 0, move onward with 1
+	dec		ecx				;if ecx was not 0, move onward with 0
+
+exit_len:
+	pop		edx
 	pop		ebx
 	mov		esp, ebp
 	pop		ebp
